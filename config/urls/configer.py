8b5f3d201ad27e36c9a023d0a81d3a026907e9a2
hj24 <huangjian24@foxmail.com>
2019-06-06 23:10:17 +0800

    📝 新增configer基类，简化代码

---
+++ config/urls/configer.py
@@ -0,0 +1,30 @@
+import json
+
+class ParseConfig:
+    """
+    解析爬虫接口
+
+    Parameters:
+        PATH - 配置文件位置
+        init_url_list - 用于保存爬虫接口的url列表
+    """
+
+    PATH = None
+
+    def __init__(self):
+        self.init_url_list = None
+
+    def parser(self):
+        """
+        解析函数
+
+        参数:
+            None
+
+        返回值:
+            返回url列表，用于初始化爬虫的任务队列
+        """
+
+        with open(self.PATH, 'r') as fo:
+            self.init_url_list = json.load(fo)
+        return self.init_url_list
