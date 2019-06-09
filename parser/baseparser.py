1826f129043d5d9acd05461ec88f18740b4b64ef
hj24 <huangjian24@foxmail.com>
2019-06-09 23:22:10 +0800

    📝 完成标题，作者，出处，日期的解析

--- parser/baseparser.py
+++ parser/baseparser.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
+from bs4 import BeautifulSoup
 
 class BaseParser(ABC):
     """
-    解析器基类，定义了一些解析器通用的方法
+    文章解析器基类，定义了一些解析器通用的方法
 
     方法:
         parse_author - 从网页中解析出作者
@@ -10,24 +11,40 @@ class BaseParser(ABC):
         parse_source - 解析文章来源
         parse_body   - 解析文章内容
         parse_link   - 解析文章链接
+    属性:
+        PARSER_TYPE  - 文档解析器类型
+        soup         - lxml解析器，可通过设置PARSER_TYPE改变
     """
+    PARSER_TYPE = 'lxml'
+    TITLE = 'h1'
+
+    def __init__(self, content):
+        self.content = content
 
     @abstractmethod
-    def parse_author(self, content):
+    def parse_title(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def parse_date(self, content):
+    def parse_author(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def parse_source(self, content):
+    def parse_date(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def parse_body(self, content):
+    def parse_source(self, *args, **kwargs):
         pass
 
     @abstractmethod
-    def parse_link(self, content):
+    def parse_body(self, *args, **kwargs):
         pass
+
+    @abstractmethod
+    def parse_link(self, *args, **kwargs):
+        pass
+
+    @property
+    def soup(self):
+        return BeautifulSoup(self.content, self.PARSER_TYPE)
