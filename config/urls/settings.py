dbd59dbd898e53a4527cea32b523cca127fddeeb
hj24 <huangjian24@foxmail.com>
2019-07-26 21:50:48 +0800

    📌 添加请求头

--- config/urls/settings.py
+++ config/urls/settings.py
@@ -10,11 +10,18 @@ GINKGO_CONFIG_FILE = os.path.join(CONFIG_PATH, 'mginkgo.json')
 # 中国知网接口文件
 ZGZW_CONFIG_FILE = os.path.join(CONFIG_PATH, 'zgzw.json')
 
-# 请求头配置
-HEADERS = {
+# 中国银杏网请求头配置
+GINKGO_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:67.0) Gecko/20100101 Firefox/67.0',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
     'Connection': 'keep-alive',
-    'Cookie': 'ASPSESSIONIDCQDSCQRA=HFAAMGGBFNGHNNPFPGDHKICC',
     'Cache-Control': 'max-age=0',
+}
+# 中国知网请求头配置
+ZGZW_HEADERS = {
+    'Accept': '*/*',
+    'Connection': 'keep-alive',
+    'Referer': 'http://m.cnki.net/mcnki/litera…ect&keyword=%E9%93%B6%E6%9D%8F',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel …) Gecko/20100101 Firefox/67.0',
+    'X-Requested-With': 'XMLHttpRequest',
 }
