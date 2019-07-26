39e00cd4cde9c3ca212023d86fb806fa12613826
hj24 <huangjian24@foxmail.com>
2019-07-26 21:41:00 +0800

    📌 添加请求头

--- config/urls/settings.py
+++ config/urls/settings.py
@@ -6,6 +6,15 @@ PARENT_PATH = os.path.abspath(os.path.dirname(__file__))
 CONFIG_PATH = os.path.join(PARENT_PATH, 'conf')
 
 # 中国银杏网接口文件
-GINKGO_CONFIG_FILE = os.path.join(CONFIG_PATH, 'ginkgo.json')
+GINKGO_CONFIG_FILE = os.path.join(CONFIG_PATH, 'mginkgo.json')
 # 中国知网接口文件
 ZGZW_CONFIG_FILE = os.path.join(CONFIG_PATH, 'zgzw.json')
+
+# 请求头配置
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:67.0) Gecko/20100101 Firefox/67.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Connection': 'keep-alive',
+    'Cookie': 'ASPSESSIONIDCQDSCQRA=HFAAMGGBFNGHNNPFPGDHKICC',
+    'Cache-Control': 'max-age=0',
+}
